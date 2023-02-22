bb35e10de2f10722c11cd3639abf36cd9c791ff5
kajolroy <shrikajol@gmail.com>
2023-02-22 10:24:06 +0600

    folder structure

---
+++ src/navigation/MainNavigation.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function MainNavigation() {
+  return (
+    <View>
+      <Text>MainNavigation</Text>
+    </View>
+  );
+}
